cfa8d44b29f4096eaf9a3ad9b11f938439f0a53e
1tryagain <m13338691652@163.com>
2023-12-02 22:29:33 +0800

    2023/12/2 实现FutureTask缓存

--- src/main/java/com/liu/SecurityApplication.java
+++ src/main/java/com/liu/SecurityApplication.java
@@ -2,7 +2,7 @@ package com.liu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
 
 /**
  * @author liu
